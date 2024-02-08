4147850841800853ac1c80d8676fa76443271f3e
Filip Sjölander <filip.sjolander@gmail.com>
2024-02-08 13:04:13 +0100

    update

--- reservation-tool/reservation-tool.py
+++ reservation-tool/reservation-tool.py
@@ -3,6 +3,7 @@ import time
 import datetime
 import requests
 import random
+import undetected_chromedriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium import webdriver
@@ -13,7 +14,7 @@ import credentials
 def generate_login_key(username, password):
     options = Options()
     options.add_argument('--headless')
-    driver = webdriver.Chrome("chromedriver_mac64/chromedriver", options=options)
+    driver = webdriver.Chrome()
     driver.get("https://cloud.timeedit.net/chalmers/web/timeedit/sso/saml2?back=https%3A%2F%2Fcloud.timeedit.net%2Fchalmers%2Fweb%2Fb1%2F")
 
     search_box = driver.find_element(By.ID, "userNameInput")
@@ -31,7 +32,7 @@ def generate_login_key(username, password):
 
     # Return only the key in 'TEchalmersweb' which is used in post requests as authentication
     return cookies[0]['value']
-
+    
 
 # Create data dictionary to pass along with request. First arg is days ahead in time
 # @start_time and @end_time is in format MMHH
@@ -39,55 +40,78 @@ def create_data_dict(name_reservation, days_ahead, room, start_time, end_time):
     date = datetime.date.today() + datetime.timedelta(days=days_ahead)
     date = '20' + str(date.strftime('%y%m%d'))
     data = {
-        'fe49': name_reservation,
-        'fe50': name_reservation + '@stud.ntnu.no',
-        'id': '-1',
-        'dates': date,
-        'datesEnd': date,
-        'startTime': start_time,
-        'endTime': end_time,
-        'o': room,
-        'url': 'https%3A%2F%2Fno.timeedit.net%2Fweb%2Fhig%2Fdb1%2Fstudent%2Fr.html%3Fh%3Dt%26sid%3D5%26id%3D-1%26step%3D2%26id%3D-'
-               + '1%26dates%3D' + date + '%26datesEnd%3D' + date + '%26startTime%3D8%253A00%26endTime%3D22%253A00%26o%3D' +
-               room + '%252C10%252C%2BA' + room + '%252C%2Bgrupperom',
-        'kind': 'reserve'}
+    'kind': 'reserve',
+    'nocache': '4',
+    'l': 'sv_SE',
+    'o': '214726.186',
+    'o': '203460.192',
+    'aos': '',
+    'dates': date,
+    'starttime': start_time,
+    'endtime': end_time,
+    'url': 'https%3A%2F%2Fcloud.timeedit.net%2Fchalmers%2Fweb%2Fb1%2Fri1Q5008.html%2300214726',
+    'fe2': '',
+    'fe8': '',
+    'CSTT': '-5267498248312575220'
+    }
     return data
 
 
 username = credentials.username
 password = credentials.password
 
-login_key = generate_login_key(username, password)
-
-
-
-dopfk
-
-
-
-room = sys.argv[3]
-start_time = sys.argv[4]
-end_time = sys.argv[5]
+room = sys.argv[1]
+start_time = sys.argv[2]
+end_time = sys.argv[3]
 # Days ahead to book the room. You should always use 15
 days_ahead = 15
-# Create cookie from arg1 (username) and arg2 (password)
+
 cookie = generate_login_key(username, password)
-# Get the room id from arg3
-room = get_room_ID(room)
+
+print(cookie)
+# Get the room id from arg1
+
 # Create the data field of the request
-data = create_data_dict(username, days_ahead, room, start_time, end_time)
+url = 'https://cloud.timeedit.net/chalmers/web/b1/ri1Q5008.html'
 
-# URL is static
-url = 'https://no.timeedit.net/web/hig/db1/student/r.html?h=t&sid=5&id=-1&step=2&id=-1&dates=20170219&datesEnd=20170219&startTime=11%3A00&endTime=11%3A05&o=162177.185%2C8%2C+A267%2C+grupperom&nocache=3'
-
-# Include cookie in headers along with user agent (dont want the website to think we are a bot)
 headers = {
-    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.100 Safari/537.36'
-    ,
-    'cookie': 'sso-parameters=back=https%3A%2F%2Fno.timeedit.net%2Fweb%2Fhig%2Fdb1%2Fstudent%2Fr.html%3Fh%3Dt%26sid%3D5%26id%3D-1&ssoserver=feide; TEwebhigdb1=' + cookie}
+    'authority': 'cloud.timeedit.net',
+    'accept': '*/*',
+    'accept-language': 'en-GB,en;q=0.9,sv;q=0.8',
+    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'cookie': 'sso-parameters=back=https%3A%2F%2Fcloud.timeedit.net%2Fchalmers%2Fweb%2Fb1%2Fri1Q5008.html&ssoserver=saml2; TEchalmersweb=' + cookie,
+    'dnt': '1',
+    'origin': 'https://cloud.timeedit.net',
+    'referer': 'https://cloud.timeedit.net/chalmers/web/b1/ri1Q5008.html',
+    'sec-ch-ua': '"Not A(Brand";v="99", "Microsoft Edge";v="121", "Chromium";v="121"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0',
+    'x-requested-with': 'XMLHttpRequest'
+}
 
-# Putting it all together in the request
-r = requests.post(url=url, headers=headers, data=data)
+print(headers.get('cookie'))
+
+data = {
+    'kind': 'reserve',
+    'nocache': '4',
+    'l': 'sv_SE',
+    'o': ['214726.186', '203460.192'],
+    'dates': '20240209',
+    'starttime': '13:45',
+    'endtime': '14:45',
+    'url': 'https://cloud.timeedit.net/chalmers/web/b1/ri1Q5008.html#00214726',
+    'fe2': '',
+    'fe8': '',
+    'CSTT': '1257603083511417094'
+}
+
+r = requests.post(url, headers=headers, data=data)
+
+print(r.text)
 answer = r.text.encode('UTF-8')
 i = 0
 # Writes response to file named according to date of reservation attempted
@@ -97,12 +121,15 @@ f = open(str(date_reservation) + "-" + sys.argv[3] + ".log", 'w')
 date_of_execution = datetime.datetime.today()
 f.write("Date executed: " + str(date_of_execution) + '\n')
 # Loop until booking successfull or message about already taken in response
-while ("innenfor dato- og klokkeslettgrensene" in answer):
+yes = True
+while (yes):
     r = requests.post(url=url, headers=headers, data=data)
     answer = r.text.encode('UTF-8')
+    print(answer)
     # Write response to file
     f.write(str(datetime.datetime.now()) + '\t' + answer + '\n')
     i += 1
+    yes = False
 time.sleep(100)
 print
 'It isnt past midnight yet, trying again...'
